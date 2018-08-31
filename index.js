1c2229784d7d060a322d7321b8ea9ddf182e13e7
jisbruzzi <jose.sbru@gmail.com>
2018-08-31 01:04:19 -0300

    Agrego el endpoint forecast, pero no está terminado (solo parcialmente) [30m]

--- index.js
+++ index.js
@@ -21,7 +21,26 @@ app.get("/weather/:city/current", (req,res)=>{
             humidity:stats.humidity
         })
     })
-    
+})
+
+app.get("/weather/:city/forecast", (req,res)=>{
+    let cityName=req.params.city.split(" ").join("%20")
+    let url="http://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&APPID="+api_key+"&units=metric"
+    console.log(url)
+    request(url,function(error,response,body){
+        console.log(error)
+        console.log(response)
+        console.log(body)
+        let stats=JSON.parse(body).list.map((e)=>{ 
+            return {
+                "dt":e.dt,
+                "temp":e.main.temp,
+                "humidity":e.main.humidity,
+                "icon":e.weather[0].icon
+            }
+        })
+        res.json(stats)
+    })
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
